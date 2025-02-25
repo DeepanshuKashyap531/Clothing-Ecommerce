145db891e66f9e6ecb035522f81c080f960dd981
Deepanshu Kashyap <deepkash877@gmail.com>
2025-02-26 01:22:04 +0530

    added Navigation Route

---
+++ dk-clothing/src/routes/navigation/navigation.components.jsx
@@ -0,0 +1,34 @@
+import { Outlet,Link } from 'react-router-dom'
+import { Fragment } from 'react';
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import './navigation.styles.scss'
+const Navigation = () => {
+    return (
+    <Fragment>
+        <div className='navigation'> 
+            <Link className='logo-container' to ='/'>
+                <CrwnLogo classname= 'logo' />
+            </Link>     
+            <div className='nav-links-container'>
+               <Link className='NavLink' to='/shop'> 
+                 SHOP
+               </Link> {/* Link is just link an anchor tag   */}
+                <Link>
+                CONTACT
+                </Link> 
+                <Link>
+                SIGN IN
+                </Link>  
+                <Link>
+                CART LOGO
+                </Link>    
+            </div>
+        </div>
+        <Outlet />
+    </Fragment>
+    
+    );
+  };
+
+
+  export default Navigation;
